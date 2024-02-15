9be3ce87ef4c126480a47cfcbf64e33364baa5bb
Guruprasadgp5178 <160009449+Guruprasadgp5178@users.noreply.github.com>
2024-02-15 18:26:01 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/moengageassignment/MainActivity.kt
+++ app/src/main/java/com/example/moengageassignment/MainActivity.kt
@@ -2,10 +2,17 @@ package com.example.moengageassignment
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.moengage.core.DataCenter
+import com.moengage.core.MoEngage
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun onCreate() {
+        super.onCreate()
+
+    	val moEngage = MoEngage.Builder(this, "SO4PCE7SVGA3BL9ATUWEOO5F", [DataCenter.DATA_CENTER_1])
+                     .build()
+        MoEngage.initialiseDefaultInstance(moEngage)
+
         setContentView(R.layout.activity_main)
     }
 }
