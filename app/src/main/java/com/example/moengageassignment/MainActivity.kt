6bd7535bd005a95ac1911cec278953db9c537a18
Guruprasadgp5178 <160009449+Guruprasadgp5178@users.noreply.github.com>
2024-02-16 16:27:27 +0530

    Update MainActivity.kt

--- app/src/main/java/com/example/moengageassignment/MainActivity.kt
+++ app/src/main/java/com/example/moengageassignment/MainActivity.kt
@@ -5,9 +5,14 @@ import android.os.Bundle
 import com.moengage.core.DataCenter
 import com.moengage.core.MoEngage
 
-class MainActivity : AppCompatActivity() {
-    override fun onCreate() {
-        super.onCreate()
-        setContentView(R.layout.activity_main)
-    }
+class MyApplication : Application() { // extends here
+override fun onCreate() { 
+super.onCreate()
+
+val moEngage = MoEngage.Builder(this, "YOUR_APP_ID", [YOUR_DATA_CENTER])
+               .build() // need to provide app id and data center
+                        // (from DataCenterclass object)
+ 
+MoEngage.initialiseDefaultInstance(moEngage)
+  }
 }
